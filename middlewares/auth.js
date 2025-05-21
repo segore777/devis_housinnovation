4953dff6a94ce8e784824cfa897866454a2ee739
segore777 <79068690+segore777@users.noreply.github.com>
2025-05-21 13:36:51 +0400

    Delete middlewares directory

--- middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ensureAuthenticated: (req, res, next) => {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        req.flash('error', 'Veuillez vous connecter');
-        res.redirect('/login');
-    }
-};
